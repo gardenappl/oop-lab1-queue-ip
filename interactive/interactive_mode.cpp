be27a66ef1524da973f45f44a54b4f1abc13ea1a
gardenappl <goldenappl@protonmail.com>
2019-11-03 20:39:26 +0200

    Interactive mode and rework IP factory

---
+++ interactive/interactive_mode.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yurik on 03.11.2019.
+//
+
+#include "interactive_mode.h"
---
+++ interactive/interactive_mode.h
@@ -0,0 +1,14 @@
+//
+// Created by yurik on 03.11.2019.
+//
+
+#ifndef OOP_LAB1_INTERACTIVE_MODE_H
+#define OOP_LAB1_INTERACTIVE_MODE_H
+
+
+class interactive_mode {
+
+};
+
+
+#endif //OOP_LAB1_INTERACTIVE_MODE_H
